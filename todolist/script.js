b62d957a2d5b52e853c001bb2978cfb8e458e484
alexander-p1 <alexanderpettersson_@hotmail.com>
2025-07-08 17:46:34 +0200

    not quite finished

--- todolist/script.js
+++ todolist/script.js
@@ -8,6 +8,14 @@ if (!todo) {
   todo = [];
 }
 
+function setLocalStorage() {
+  localStorage.setItem("todo-list", JSON.stringify(todo));
+}
+
+function setAlertMessage(message) {
+  todoAlert.innerText = message;
+}
+
 // Function for updating
 const createTodoItems = () => {
   if (todoValue.value === "") {
@@ -43,3 +51,123 @@ const createTodoItems = () => {
   todoValue.value = "";
   setAlertMessage("En todo skapad!");
 };
+
+// Read todos
+function ReadToDoItems() {
+  todo.forEach((element) => {
+    let li = document.createElement("li");
+    let style = "";
+    if (element.status) {
+      style = "style='text-decoration: line-through'";
+    }
+    const todoItems = `<div ${style} title="Dubbelklicka för att checka av" ondblclick="CompletedToDoItems(this)">${
+      element.item
+    }
+    ${
+      style === ""
+        ? ""
+        : '<img class="todo-controls" src="" />'
+    }</div><div>
+    ${
+      style === ""
+        ? '<img class="edit todo-controls" onclick="UpdateToDoItems(this)" src="" />'
+        : ""
+    }
+    <img class="delete todo-controls" onclick="DeleteToDoItems(this)" src="" /></div></div>`;
+    li.innerHTML = todoItems;
+    listItems.appendChild(li);
+  });
+}
+ReadToDoItems();
+
+// Update todos
+function UpdateToDoItems(e) {
+  if (
+    e.parentElement.parentElement.querySelector("div").style.textDecoration ===
+    ""
+  ) {
+    todoValue.value =
+      e.parentElement.parentElement.querySelector("div").innerText;
+    updateText = e.parentElement.parentElement.querySelector("div");
+    addUpdate.setAttribute("onclick", "UpdateOnSelectionItems()");
+    addUpdate.setAttribute("src", "/images/refresh.png");
+    todoValue.focus();
+  }
+}
+
+function UpdateOnSelectionItems() {
+  let IsPresent = false;
+  todo.forEach((element) => {
+    if (element.item == todoValue.value) {
+      IsPresent = true;
+    }
+  });
+
+  if (IsPresent) {
+    setAlertMessage("Detta finns redan!");
+    return;
+  }
+
+  todo.forEach((element) => {
+    if (element.item == updateText.innerText.trim()) {
+      element.item = todoValue.value;
+    }
+  });
+  setLocalStorage();
+
+  updateText.innerText = todoValue.value;
+  addUpdate.setAttribute("onclick", "CreateToDoItems()");
+  todoValue.value = "";
+  setAlertMessage("Todo uppdaterad!");
+}
+
+// Delete todos
+function DeleteToDoItems(e) {
+  let deleteValue =
+    e.parentElement.parentElement.querySelector("div").innerText;
+
+  if (confirm(`Är du säker på att du vill ta bort denna todo?`)) {
+    e.parentElement.parentElement.setAttribute("class", "deleted-item");
+    todoValue.focus();
+
+    todo.forEach((element) => {
+      if (element.item == deleteValue.trim()) {
+        todo.splice(element, 1);
+      }
+    });
+
+    setTimeout(() => {
+      e.parentElement.parentElement.remove();
+    }, 1000);
+
+    setLocalStorage();
+  }
+}
+
+function CompletedToDoItems(e) {
+  if (e.parentElement.querySelector("div").style.textDecoration === "") {
+    e.parentElement.querySelector("div").style.textDecoration = "line-through";
+
+    todo.forEach((element) => {
+      if (
+        e.parentElement.querySelector("div").innerText.trim() == element.item
+      ) {
+        element.status = true;
+      }
+    });
+    setLocalStorage();
+    setAlertMessage("Todo item Completed Successfully!");
+  }
+}
+
+function setLocalStorage() {
+  localStorage.setItem("todo-list", JSON.stringify(todo));
+}
+
+function setAlertMessage(message) {
+  todoAlert.removeAttribute("class");
+  todoAlert.innerText = message;
+  setTimeout(() => {
+    todoAlert.classList.add("toggleMe");
+  }, 1000);
+}
